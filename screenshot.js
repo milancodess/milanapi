9ad4b970e1e23fcf5e23759146d2352cd9a50828
Milan Bhandari <108926680+milancodess@users.noreply.github.com>
2024-01-28 00:45:39 +0545

    Create screenshot.js

---
+++ screenshot.js
@@ -0,0 +1,19 @@
+import https from "https";
+
+export function createScreenshot(url) {
+  return new Promise((resolve, reject) => {
+    const screenshotURL = `https://image.thum.io/get/width/1920/crop/400/fullpage/noanimate/?url=${encodeURIComponent(url)}`;
+
+    https
+      .get(screenshotURL, (response) => {
+        if (response.statusCode === 200) {
+          resolve(response);
+        } else {
+          reject(new Error(`Failed to generate screenshot. Status code: ${response.statusCode}`));
+        }
+      })
+      .on("error", (error) => {
+        reject(error);
+      });
+  });
+}
