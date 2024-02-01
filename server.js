8ff29970df25e780178afcbfcdafe0dafd6e9a12
Milan Bhandari <108926680+milancodess@users.noreply.github.com>
2024-02-02 02:47:43 +0545

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import fileUpload from 'express-fileupload';
+import { Prodia } from "prodia.js";
 import bodyParser from 'body-parser';
 import multer from 'multer';
 import stream from 'stream';
@@ -24,6 +25,7 @@ import rateLimit from "express-rate-limit";
 import path from "path";
 import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const prodia = new Prodia("fd8c21c4-a464-4e3e-a0cc-6d7cc4f32dd3");
 const port = process.env.PORT || 5000;
 const app = express();
 const wrapText = async (ctx, text, maxWidth) => {
@@ -1267,6 +1269,173 @@ axios.post(apiUrl, requestData, { headers })
   }
 });
 
+app.get('/imagine', async (req, res) => {
+  const prompt = req.query.prompt;
+  const model = req.query.model;
+
+  if (!prompt) {
+    return res.status(400).json({ error: 'Please provide a prompt.' });
+  }
+  const nsfwPath = path.join('nsfw.json');
+const nsfwWords = JSON.parse(fs.readFileSync(nsfwPath, 'utf8'));
+  const forbiddenWords = nsfwWords;
+  const antiNSFW = 'https://i.ibb.co/hyNnCtm/20230712-023348.jpg';
+  for (const word of forbiddenWords) {
+    if (prompt.includes(word)) {
+      return res.json({
+        combinedImageUrl: antiNSFW,
+        imageUrls: { image1: antiNSFW, image2: antiNSFW, image3: antiNSFW, image4: antiNSFW }
+      });
+    }
+  }
+
+  try {
+    const imageUrls = await generateImages(prompt.toString(), model);
+    const combinedImagePath = await combineImages(imageUrls);
+    const author = {
+      name: "MilanXD",
+      contact: "https://www.facebook.com/milanxd.bh"
+    };
+    
+    const modifiedImageUrls = imageUrls.reduce((acc, curr, index) => {
+      acc[`image${index + 1}`] = curr;
+      return acc;
+    }, {});
+
+    return res.json({ combinedImageUrl: "https://milanbhandari.imageapi.repl.co/milanxd/combinedImage.jpg", imageUrls: modifiedImageUrls, author });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'An error occurred while generating the image.' });
+  }
+});
+
+async function generateImages(prompt, model) {
+  const models = [
+  "3Guofeng3_v34.safetensors [50f420de]",
+  "absolutereality_V16.safetensors [37db0fc3]",
+  "absolutereality_v181.safetensors [3d9d4d2b]",
+  "amIReal_V41.safetensors [0a8a2e61]",
+  "analog-diffusion-1.0.ckpt [9ca13f02]",
+  "anythingv3_0-pruned.ckpt [2700c435]",
+  "anything-v4.5-pruned.ckpt [65745d25]",
+  "anythingV5_PrtRE.safetensors [893e49b9]",
+  "AOM3A3_orangemixs.safetensors [9600da17]",
+  "blazing_drive_v10g.safetensors [ca1c1eab]",
+  "cetusMix_Version35.safetensors [de2f2560]",
+  "childrensStories_v13D.safetensors [9dfaabcb]",
+  "childrensStories_v1SemiReal.safetensors [a1c56dbb]",
+  "childrensStories_v1ToonAnime.safetensors [2ec7b88b]",
+  "Counterfeit_v30.safetensors [9e2a8f19]",
+  "cuteyukimixAdorable_midchapter3.safetensors [04bdffe6]",
+  "cyberrealistic_v33.safetensors [82b0d085]",
+  "dalcefo_v4.safetensors [425952fe]",
+  "deliberate_v2.safetensors [10ec4b29]",
+  "deliberate_v3.safetensors [afd9d2d4]",
+  "dreamlike-anime-1.0.safetensors [4520e090]",
+  "dreamlike-diffusion-1.0.safetensors [5c9fd6e0]",
+  "dreamlike-photoreal-2.0.safetensors [fdcf65e7]",
+  "dreamshaper_6BakedVae.safetensors [114c8abb]",
+  "dreamshaper_7.safetensors [5cf5ae06]",
+  "dreamshaper_8.safetensors [9d40847d]",
+  "edgeOfRealism_eorV20.safetensors [3ed5de15]",
+  "EimisAnimeDiffusion_V1.ckpt [4f828a15]",
+  "elldreths-vivid-mix.safetensors [342d9d26]",
+  "epicrealism_naturalSinRC1VAE.safetensors [90a4c676]",
+  "ICantBelieveItsNotPhotography_seco.safetensors [4e7a3dfd]",
+  "juggernaut_aftermath.safetensors [5e20c455]",
+  "lofi_v4.safetensors [ccc204d6]",
+  "lyriel_v16.safetensors [68fceea2]",
+  "majicmixRealistic_v4.safetensors [29d0de58]",
+  "mechamix_v10.safetensors [ee685731]",
+  "meinamix_meinaV9.safetensors [2ec66ab0]",
+  "meinamix_meinaV11.safetensors [b56ce717]",
+  "neverendingDream_v122.safetensors [f964ceeb]",
+  "openjourney_V4.ckpt [ca2f377f]",
+  "pastelMixStylizedAnime_pruned_fp16.safetensors [793a26e8]",
+  "portraitplus_V1.0.safetensors [1400e684]",
+  "protogenx34.safetensors [5896f8d5]",
+  "Realistic_Vision_V1.4-pruned-fp16.safetensors [8d21810b]",
+  "Realistic_Vision_V2.0.safetensors [79587710]",
+  "Realistic_Vision_V4.0.safetensors [29a7afaa]",
+  "Realistic_Vision_V5.0.safetensors [614d1063]",
+  "redshift_diffusion-V10.safetensors [1400e684]",
+  "revAnimated_v122.safetensors [3f4fefd9]",
+  "rundiffusionFX25D_v10.safetensors [cd12b0ee]",
+  "rundiffusionFX_v10.safetensors [cd4e694d]",
+  "sdv1_4.ckpt [7460a6fa]",
+  "v1-5-pruned-emaonly.safetensors [d7049739]",
+  "v1-5-inpainting.safetensors [21c7ab71]",
+  "shoninsBeautiful_v10.safetensors [25d8c546]",
+  "theallys-mix-ii-churned.safetensors [5d9225a4]",
+  "timeless-1.0.ckpt [7c4971d4]",
+  "toonyou_beta6.safetensors [980f6b15]"
+  "dreamshaperXL10_alpha2.safetensors [c8afe2ef]",
+  "dynavisionXL_0411.safetensors [c39cc051]",
+  "juggernautXL_v45.safetensors [e75f5471]",
+  "realismEngineSDXL_v10.safetensors [af771c3f]",
+  "sd_xl_base_1.0.safetensors [be9edd61]",
+  "sd_xl_base_1.0_inpainting_0.1.safetensors [5679a81a]",
+  "turbovisionXL_v431.safetensors [78890989]"
+];
+
+  const selectedModel = model ? models[Number(model) - 1] : "absolutereality_v181.safetensors [3d9d4d2b]";
+
+  if (!selectedModel) {
+    throw new Error("Invalid model parameter");
+  }
+
+  const imagePromises = [];
+
+  for (let i = 0; i < 4; i++) {
+    let job = await prodia.createJob({
+      model: selectedModel,
+      prompt: prompt,
+      seed: -1,
+      negative_prompt: "",
+      aspect_ratio: "square"
+    });
+
+    while (job.status !== "succeeded") {
+      await new Promise((resolve) => setTimeout(resolve, 250));
+      job = await prodia.getJob(job.job);
+    }
+
+    imagePromises.push(job.imageUrl);
+  }
+
+  return Promise.all(imagePromises);
+}
+
+ async function combineImages(imageUrls) {
+  const images = await Promise.all(imageUrls.map(url => Jimp.read(url)));
+
+  const imageWidth = images[0].bitmap.width;
+  const imageHeight = images[0].bitmap.height;
+
+  const combinedImageWidth = imageWidth * 2;
+  const combinedImageHeight = imageHeight * 2;
+
+  const combinedImage = new Jimp(combinedImageWidth, combinedImageHeight);
+
+  const image1X = 0;
+  const image1Y = 0;
+  const image2X = imageWidth;
+  const image2Y = 0;
+  const image3X = 0;
+  const image3Y = imageHeight;
+  const image4X = imageWidth;
+  const image4Y = imageHeight;
+
+  combinedImage.composite(images[0], image1X, image1Y);
+  combinedImage.composite(images[1], image2X, image2Y);
+  combinedImage.composite(images[2], image3X, image3Y);
+  combinedImage.composite(images[3], image4X, image4Y);
+
+  const combinedImgPath = './public/combinedImage.jpg'; // Path to save the combined image
+  await combinedImage.writeAsync(combinedImgPath);
+
+  return combinedImgPath;
+              } 
 
 app.listen(port, "0.0.0.0", function () {
     console.log(`Listening on port ${port}`)
