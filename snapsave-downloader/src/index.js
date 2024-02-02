c466ae6581d99ba007b0e75887a2f6cfb8a3ccf6
Milan Bhandari <108926680+milancodess@users.noreply.github.com>
2024-02-02 15:34:11 +0545

    Update index.js

--- snapsave-downloader/src/index.js
+++ snapsave-downloader/src/index.js
@@ -1,23 +1,24 @@
-import axios from "axios";
-import cheerio from "cheerio";
-module.exports = snapsave = (url) => {
+import axios from 'axios';
+import cheerio from 'cheerio';
+
+export const snapsave = (url: string): Promise<any> => {
   return new Promise(async (resolve) => {
     try {
-      if (!url.match(/(?:https?:\/\/(web\.|www\.|m\.)?(facebook|fb)\.(com|watch)\S+)?$/) && !url.match(/(https|http):\/\/www.instagram.com\/(p|reel|tv|stories)/gi)) {
+      if (
+        !url.match(/(?:https?:\/\/(web\.|www\.|m\.)?(facebook|fb)\.(com|watch)\S+)?$/) &&
+        !url.match(/(https|http):\/\/www.instagram.com\/(p|reel|tv|stories)/gi)
+      ) {
         return resolve({ developer: '@Milan Bhandari', status: false, msg: `Link Url not valid` });
       }
 
-      function decodeSnapApp(args) {
+      function decodeSnapApp(args: string[]) {
         let [h, u, n, t, e, r] = args;
-        // @ts-ignore
-        function decode(d, e, f) {
+        function decode(d: string, e: number, f: number) {
           const g = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/'.split('');
           let h = g.slice(0, e);
           let i = g.slice(0, f);
-          // @ts-ignore
           let j = d.split('').reverse().reduce(function (a, b, c) {
-            if (h.indexOf(b) !== -1)
-              return a += h.indexOf(b) * (Math.pow(e, c));
+            if (h.indexOf(b) !== -1) return (a += h.indexOf(b) * Math.pow(e, c));
           }, 0);
           let k = '';
           while (j > 0) {
@@ -26,53 +27,57 @@ module.exports = snapsave = (url) => {
           }
           return k || '0';
         }
-        r = '';
+        let r = '';
         for (let i = 0, len = h.length; i < len; i++) {
-          let s = "";
-          // @ts-ignore
+          let s = '';
           while (h[i] !== n[e]) {
             s += h[i];
             i++;
           }
-          for (let j = 0; j < n.length; j++)
-            s = s.replace(new RegExp(n[j], "g"), j.toString());
-          // @ts-ignore
+          for (let j = 0; j < n.length; j++) s = s.replace(new RegExp(n[j], 'g'), j.toString());
           r += String.fromCharCode(decode(s, e, 10) - t);
         }
         return decodeURIComponent(encodeURIComponent(r));
       }
 
-      function getEncodedSnapApp(data) {
-        return data.split('decodeURIComponent(escape(r))}(')[1]
+      function getEncodedSnapApp(data: string) {
+        return data
+          .split('decodeURIComponent(escape(r))}(')[1]
           .split('))')[0]
           .split(',')
-          .map(v => v.replace(/"/g, '').trim());
+          .map((v) => v.replace(/"/g, '').trim());
       }
 
-      function getDecodedSnapSave(data) {
-        return data.split('getElementById("download-section").innerHTML = "')[1]
+      function getDecodedSnapSave(data: string) {
+        return data
+          .split('getElementById("download-section").innerHTML = "')[1]
           .split('"; document.getElementById("inputData").remove(); ')[0]
           .replace(/\\(\\)?/g, '');
       }
 
-      function decryptSnapSave(data) {
+      function decryptSnapSave(data: string) {
         return getDecodedSnapSave(decodeSnapApp(getEncodedSnapApp(data)));
       }
 
-      const response = await axios.post('https://snapsave.app/action.php?lang=id', `url=${url}`, {
-        headers: {
-          'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-          'content-type': 'application/x-www-form-urlencoded',
-          'origin': 'https://snapsave.app',
-          'referer': 'https://snapsave.app/id',
-          'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
+      const response = await axios.post(
+        'https://snapsave.app/action.php?lang=id',
+        `url=${url}`,
+        {
+          headers: {
+            accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'content-type': 'application/x-www-form-urlencoded',
+            origin: 'https://snapsave.app',
+            referer: 'https://snapsave.app/id',
+            'user-agent':
+              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+          },
         }
-      });
+      );
       const html = response.data;
 
       const decode = decryptSnapSave(html);
       const $ = cheerio.load(decode);
-      const results = [];
+      const results: any[] = [];
 
       if ($('table.table').length || $('article.media > figure').length) {
         const thumbnail = $('article.media > figure').find('img').attr('src');
@@ -89,7 +94,7 @@ module.exports = snapsave = (url) => {
             resolution,
             thumbnail,
             url: _url,
-            shouldRender
+            shouldRender,
           });
         });
       } else {
@@ -100,7 +105,7 @@ module.exports = snapsave = (url) => {
             if (!/https?:\/\//.test(_url || '')) _url = `https://snapsave.app${_url}`;
             results.push({
               thumbnail,
-              url: _url
+              url: _url,
             });
           });
         });
