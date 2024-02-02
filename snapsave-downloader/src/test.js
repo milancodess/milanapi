796004ba35d30c171ed8dd1becf5da2ba69f07f7
Milan Bhandari <108926680+milancodess@users.noreply.github.com>
2024-02-02 15:35:06 +0545

    Update test.js

--- snapsave-downloader/src/test.js
+++ snapsave-downloader/src/test.js
@@ -1,27 +1,31 @@
-const snapsave = require("./index")
+import { snapsave } from "./index";
 
-async function test(url){
-    let result = await snapsave(url)
-    return result
+async function test(url: string) {
+  try {
+    let result = await snapsave(url);
+    return result;
+  } catch (err) {
+    throw err;
+  }
 }
 
-test("https://www.instagram.com/p/CMAMhvgsVal/").then(result=>{
-    console.log("Test Videos/Images OK")
-    console.log(result)
-}).catch(err=>{
-    console.error(err)
-})
+test("https://www.instagram.com/p/CMAMhvgsVal/").then((result) => {
+  console.log("Test Videos/Images OK");
+  console.log(result);
+}).catch((err) => {
+  console.error(err);
+});
 
-test("https://www.instagram.com/p/CHSvvKXpkH6/").then(result=>{
-    console.log("Test Only Image OK")
-    console.log(result)
-}).catch(err=>{
-    console.error(err)
-})
+test("https://www.instagram.com/p/CHSvvKXpkH6/").then((result) => {
+  console.log("Test Only Image OK");
+  console.log(result);
+}).catch((err) => {
+  console.error(err);
+});
 
-test("https://www.instagram.com/tv/CdmYaq3LAYo/").then(result=>{
-    console.log("Test Only Video OK")
-    console.log(result)
-}).catch(err=>{
-    console.error(err)
-})
+test("https://www.instagram.com/tv/CdmYaq3LAYo/").then((result) => {
+  console.log("Test Only Video OK");
+  console.log(result);
+}).catch((err) => {
+  console.error(err);
+});
