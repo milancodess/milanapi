03085d07698e1f5170f70c28044681552296e836
Milan Bhandari <108926680+milancodess@users.noreply.github.com>
2024-07-14 21:40:54 +0545

    Update server.js

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ const milan = [
 ];
 
 app.get("/", (req, res) => {
-res.sendFile(path.join(__dirname, "dashboard","home.html"));
+res.sendFile(path.join(__dirname, "dashboard","instadownloader.html"));
 });
 
 app.get("/home", (req, res) => {
@@ -1725,49 +1725,121 @@ app.get('/twitter', async (req, res) => {
   }
 });
 
-app.get('/reddit', async (req, res) => {
-    const { url } = req.query;
-
-    if (!url) {
-        return res.status(400).json({ error: 'Missing required query parameter: url' });
+const snapsave = async (url) => {
+  try {
+    if (!url.match(/(?:https?:\/\/(web\.|www\.|m\.)?(facebook|fb)\.(com|watch)\S+)?$/) && !url.match(/(https|http):\/\/www.instagram.com\/(p|reel|tv|stories)/gi)) {
+      return { developer: '@Milan Bhandari', status: false, msg: 'Link URL not valid' };
     }
 
-    try {
-        const response = await fetch("https://redvid.io/en/fetch", {
-            headers: {
-                "accept": "*/*",
-                "accept-language": "en-US,en;q=0.9",
-                "content-type": "application/json",
-                "sec-ch-ua": "\"Not-A.Brand\";v=\"99\", \"Chromium\";v=\"124\"",
-                "sec-ch-ua-mobile": "?1",
-                "sec-ch-ua-platform": "\"Android\"",
-                "sec-fetch-dest": "empty",
-                "sec-fetch-mode": "cors",
-                "sec-fetch-site": "same-origin",
-                "cookie": "cf_clearance=DYcfA7D_23kA8pXkSb5XxbBMXzvGgbtJpjvK_zEhFuk-1720723662-1.0.1.1-XYywtq1Z9Ofvx4uUDgY_Ltwjgp.DmLE_Al3167AQaWATBkRztNW6gGHEl38d8wr4rd13eROmLDBPyZZezgcOVw; XSRF-TOKEN=eyJpdiI6ImsrVEoxS3Vkc1FHNVNLbTVHaTF4RUE9PSIsInZhbHVlIjoiQlpIVVJtcW5xTU9mOTZEV0RHSStML0l6bHRNd3ZCYWwwaDdxaG9uMGVTZlBHY1UyRXFvZ2pLNTBFbG1nb0k1SDdJb2JXSVYxTG9GU0tFS2Y4WlRMN21DOE1OQkhScm5JMkdSaFgyM2cvZTRHMllraW5PRWxqWU5uTUczcFpoaksiLCJtYWMiOiJkZWUwNmM4MGE4NzdjZThkZWFkMzEwMjA2OTczY2MwZTEyOWNlZTY0ZjhlZDEyODRjMDQ0MzcyM2M5YmYxYTJiIiwidGFnIjoiIn0%3D; laravel_session=eyJpdiI6Ik9jZXdBQ3pFcUJ0U1FqV2RDWkIwUEE9PSIsInZhbHVlIjoiT1ZqU3k1blBTMDJIMUpVUU9xbDVFK2E4bzY4NW1VR1JZbTFFSmlnRkVzNjRaNW92UHBac1YwL2djUzUrd0dtWlNKMXpROWczVGFUZDB6N2Yrb3o1SUFERlJmdWY1THE4cEZiNnUrYzVURTR4aWNMR3REZS9EeDAxQThtakZXcDYiLCJtYWMiOiJhNjViM2NmNDQxYWViOGExNGM0NjMyNzI1NGJmMWVlZDg1NmFkOTc4NTIxOTBjZGRhNmRiY2RjYjYzODgwYTllIiwidGFnIjoiIn0%3D",
-                "Referer": "https://redvid.io/",
-                "Referrer-Policy": "strict-origin-when-cross-origin"
-            },
-            body: JSON.stringify({
-                url: url,
-                _token: "5MIHDbW0HvGuhmT2GnfarhQUxoEWvqafD1tb9Yza"
-            }),
-            method: "POST"
-        });
-
-        const contentType = response.headers.get('content-type');
-
-        let data;
-        if (contentType && contentType.includes('application/json')) {
-            data = await response.json();
-        } else {
-            data = await response.text();
+    function decodeSnapApp(args) {
+      let [h, u, n, t, e, r] = args;
+      function decode(d, e, f) {
+        const g = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/'.split('');
+        let h = g.slice(0, e);
+        let i = g.slice(0, f);
+        let j = d.split('').reverse().reduce((a, b, c) => {
+          if (h.indexOf(b) !== -1)
+            return a += h.indexOf(b) * (Math.pow(e, c));
+        }, 0);
+        let k = '';
+        while (j > 0) {
+          k = i[j % f] + k;
+          j = (j - (j % f)) / f;
         }
-
-        res.json({ data });
-    } catch (error) {
-        res.status(500).json({ error: 'An error occurred', details: error.message });
+        return k || '0';
+      }
+      r = '';
+      for (let i = 0, len = h.length; i < len; i++) {
+        let s = "";
+        while (h[i] !== n[e]) {
+          s += h[i];
+          i++;
+        }
+        for (let j = 0; j < n.length; j++)
+          s = s.replace(new RegExp(n[j], "g"), j.toString());
+        r += String.fromCharCode(decode(s, e, 10) - t);
+      }
+      return decodeURIComponent(encodeURIComponent(r));
     }
+
+    function getEncodedSnapApp(data) {
+      return data.split('decodeURIComponent(escape(r))}(')[1]
+        .split('))')[0]
+        .split(',')
+        .map(v => v.replace(/"/g, '').trim());
+    }
+
+    function getDecodedSnapSave(data) {
+      return data.split('getElementById("download-section").innerHTML = "')[1]
+        .split('"; document.getElementById("inputData").remove(); ')[0]
+        .replace(/\\(\\)?/g, '');
+    }
+
+    function decryptSnapSave(data) {
+      return getDecodedSnapSave(decodeSnapApp(getEncodedSnapApp(data)));
+    }
+
+    const response = await axios.post('https://snapsave.app/action.php?lang=id', `url=${url}`, {
+      headers: {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'content-type': 'application/x-www-form-urlencoded',
+        'origin': 'https://snapsave.app',
+        'referer': 'https://snapsave.app/id',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
+      }
+    });
+
+    const html = response.data;
+    const decode = decryptSnapSave(html);
+    const $ = cheerio.load(decode);
+    const results = [];
+
+    if ($('table.table').length || $('article.media > figure').length) {
+      const thumbnail = $('article.media > figure').find('img').attr('src');
+      $('tbody > tr').each((_, el) => {
+        const $el = $(el);
+        const $td = $el.find('td');
+        const resolution = $td.eq(0).text();
+        let _url = $td.eq(2).find('a').attr('href') || $td.eq(2).find('button').attr('onclick');
+        const shouldRender = /get_progressApi/ig.test(_url || '');
+        if (shouldRender) {
+          _url = /get_progressApi\('(.*?)'\)/.exec(_url || '')?.[1] || _url;
+        }
+        results.push({
+          resolution,
+          thumbnail,
+          url: _url,
+          shouldRender
+        });
+      });
+    } else {
+      $('div.download-items__thumb').each((_, tod) => {
+        const thumbnail = $(tod).find('img').attr('src');
+        $('div.download-items__btn').each((_, ol) => {
+          let _url = $(ol).find('a').attr('href');
+          if (!/https?:\/\//.test(_url || '')) _url = `https://snapsave.app${_url}`;
+          results.push({
+            thumbnail,
+            url: _url
+          });
+        });
+      });
+    }
+
+    if (!results.length) {
+      return { developer: '@Milan Bhandari', status: false, msg: 'Blank data' };
+    }
+
+    return { developer: '@Milan Bhandari', status: true, data: results };
+  } catch (e) {
+    return { developer: '@Milan Bhandari', status: false, msg: e.message };
+  }
+};
+
+app.post('/instadownloader , async (req, res) => {
+  const { url } = req.body;
+  const result = await snapsave(url);
+  res.json(result);
 });
 
 app.listen(port, "0.0.0.0", function () {
