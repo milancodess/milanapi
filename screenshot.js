21a3c55ac6fddeae096882f95b99996034be2181
Milan Bhandari <108926680+milancodess@users.noreply.github.com>
2024-08-08 09:45:04 +0545

    Update screenshot.js

--- screenshot.js
+++ screenshot.js
@@ -1,19 +1,29 @@
 const https = require("https");
 
-export function createScreenshot(url) {
+function createScreenshot(url) {
   return new Promise((resolve, reject) => {
     const screenshotURL = `https://image.thum.io/get/width/1920/crop/400/fullpage/noanimate/?url=${encodeURIComponent(url)}`;
 
     https
       .get(screenshotURL, (response) => {
-        if (response.statusCode === 200) {
-          resolve(response);
-        } else {
-          reject(new Error(`Failed to generate screenshot. Status code: ${response.statusCode}`));
-        }
+        let data = [];
+
+        response.on('data', (chunk) => {
+          data.push(chunk);
+        });
+
+        response.on('end', () => {
+          if (response.statusCode === 200) {
+            resolve(Buffer.concat(data));
+          } else {
+            reject(new Error(`Failed to generate screenshot. Status code: ${response.statusCode}`));
+          }
+        });
       })
       .on("error", (error) => {
         reject(error);
       });
   });
 }
+
+module.exports = createScreenshot;
