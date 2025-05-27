afcb1bca3c455ed6b2a8a4654fa92bbd7cca187c
Milan Bhandari <108926680+milancodess@users.noreply.github.com>
2025-05-28 04:12:26 +0545

    Create middleware.js

---
+++ middleware.js
@@ -0,0 +1,13 @@
+export function middleware(request) {
+  const url = new URL(request.url);
+
+  if (
+    request.headers.get("host") === "www.bhandarimilan.info.np" &&
+    url.pathname.startsWith("/api")
+  ) {
+    url.hostname = "www.api.bhandarimilan.info.np";
+    return Response.redirect(url.toString(), 301);
+  }
+
+  return new Response(null, { status: 204 });
+}
